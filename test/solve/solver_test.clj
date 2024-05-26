6550605679a8c14f044e26c015754d2ec0577517
Davin Tryon <davin@lockdown-2.cust.communityfibre.co.uk>
2024-05-26 12:28:36 +0100

    try a 4x4

--- test/solve/solver_test.clj
+++ test/solve/solver_test.clj
@@ -14,7 +14,7 @@
                          [["x" "r" "r"]
                           ["e" "a" "e"]
                           ["t" "l" "i"]] 
-                          )))
+                          ))))
 
 (deftest test-get-value
   (do
@@ -22,17 +22,17 @@
       (is (= (sut/get-value [0 0] grid) "a"))
       (is (= (sut/get-value [0 1] grid) "i"))
       (is (= (sut/get-value [1 0] grid) "d"))
-      (is (= (sut/get-value [1 1] grid) "e"))))
+      (is (= (sut/get-value [1 1] grid) "e")))))
 
 (deftest test-all-points-grid
   (do
     (let [grid (sut/make-grid 2 ["a" "d" "i" "e"])]
-      (is (= (sut/all-points-grid grid) [[[0 0] [0 1]] [[1 0] [1 1]]]))))
+      (is (= (sut/all-points-grid grid) [[[0 0] [0 1]] [[1 0] [1 1]]])))))
 
 (deftest test-all-points
   (do
     (let [grid (sut/make-grid 2 ["a" "d" "i" "e"])]
-      (is (= (sut/all-points grid) [[0 0] [0 1] [1 0] [1 1]]))))
+      (is (= (sut/all-points grid) [[0 0] [0 1] [1 0] [1 1]])))))
 
 (deftest test-valid-point
   (do
@@ -43,56 +43,64 @@
       (is (sut/valid-point [0 0] 1))
       (is (sut/valid-point [1 0] 1))
       (is (sut/valid-point [0 1] 1))
-      (is (sut/valid-point [0 0] 1)))
+      (is (sut/valid-point [0 0] 1))))
 
 (deftest test-get-neighbors
   (do
       (is (= (sut/get-neighbors [0 0] [[[0 0] [0 1]] [[1 0] [1 1]]]) [[0 1] [1 0] [1 1]]))
-      (is (= (sut/get-neighbors [1 0] [[[0 0] [0 1]] [[1 0] [1 1]]]) [[1 1] [0 0] [0 1]])))
+      (is (= (sut/get-neighbors [1 0] [[[0 0] [0 1]] [[1 0] [1 1]]]) [[1 1] [0 0] [0 1]]))))
 
 (deftest test-already-used-point
   (do
       (is (sut/already-used-point [0 1] [[0 0] [0 1] [1 1]]))
       (is (not (sut/already-used-point [1 0] [[0 0] [0 1] [1 1]])))
-      (is (sut/already-used-point [0 0] [[0 0] [0 1] [1 1]])))
+      (is (sut/already-used-point [0 0] [[0 0] [0 1] [1 1]]))))
 
 (deftest test-get-unused-neighbors
   (do
       (is (= (sut/get-unused-neighbors [0 0] [] [[[0 0] [0 1]] [[1 0] [1 1]]]) [[0 1] [1 0] [1 1]]))
-      (is (= (sut/get-unused-neighbors [1 0] [[0 0]] [[[0 0] [0 1]] [[1 0] [1 1]]]) [[1 1] [0 1]])))
+      (is (= (sut/get-unused-neighbors [1 0] [[0 0]] [[[0 0] [0 1]] [[1 0] [1 1]]]) [[1 1] [0 1]]))))
 
 (deftest test-get-point-paths
   (do
-      (is (= (count (sut/get-point-paths [1 0] [[[0 0] [0 1]] [[1 0] [1 1]]])) 6)))
+      (is (= (count (sut/get-point-paths [1 0] [[[0 0] [0 1]] [[1 0] [1 1]]])) 6))))
 
 (deftest test-all-paths
   (do
     (let [grid (sut/make-grid 2 ["a" "d" "i" "e"])]
-      (is (= (count (sut/all-paths grid)) 24))))
+      (is (= (count (sut/all-paths grid)) 24)))))
 
 (deftest test-all-paths3
   (do
     (let [grid (sut/make-grid 3 ["x" "r" "r" "e" "a" "e" "t" "l" "i"])]
-      (is (= (count (sut/all-paths grid)) 4224))))
+      (is (= (count (sut/all-paths grid)) 4224)))))
 
 (deftest test-paths-words
   (do
     (let [grid (sut/make-grid 2 ["a" "d" "i" "e"])
           paths (sut/all-paths grid)]
-      (is (= (count (sut/get-paths-words grid paths)) 24))))
+      (is (= (count (sut/get-paths-words grid paths)) 24)))))
 
 (deftest test-get-words
   (do
     (let [grid (sut/make-grid 2 ["a" "d" "i" "e"])]
-      (is (= (sut/get-words grid 4) [{4 #{"idea"}}]))))
+      (is (= (sut/get-words grid 4) [{4 #{"idea"}}])))))
 
 (deftest test-get-words2
   (do
     (let [grid (sut/make-grid 2 ["i" "t" "m" "e"])]
-      (is (= (sut/get-words grid 3) [{3 #{"tie" "met"}} {4 #{"item" "time" "mite" "emit"}}]))))
+      (is (= (sut/get-words grid 3) [{3 #{"tie" "met"}} {4 #{"item" "time" "mite" "emit"}}])))))
 
 (deftest test-squaredle
   (do 
-    (is (= (sut/squaredle 3 ["x" "r" "r" "e" "a" "e" "t" "l" "i"] 4) nil)))
+    (is (= (sut/squaredle 3 ["x" "r" "r" "e" "a" "e" "t" "l" "i"] 4) nil))))
+
+(deftest test-squaredle
+  (do 
+    (is (= (sut/squaredle 3 ["t" "m" "r" "e" "n" "a" "w" "g" "g"] 4) nil))))
+
+(deftest test-squaredle
+  (do 
+    (is (= (sut/squaredle 4 ["b" "o" "t" "h" "w" "h" "i" "f" "e" "r" "d" "n" "t" "l" "a" "g"] 4) nil))))
 
 
